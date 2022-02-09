907cbd8606ec0557fef2702222b4e7c59abc4f0e
JackeyLove1 <1572727410@qq.com>
2022-02-09 11:29:04 +0800

    2022-02-09

---
+++ ACM/template/前缀和.cpp
@@ -0,0 +1,84 @@
+// 二维前缀和的修改求和
+// 前缀和求和
+for (int i = 1; i <= n; i ++ )
+        for (int j = 1; j <= m; j ++ )
+            s[i][j] += s[i - 1][j] + s[i][j - 1] - s[i - 1][j - 1];
+
+// 二维差分，计算区间和和修改区间
+#include <iostream>
+
+using namespace std;
+
+const int N = 1010;
+
+int n, m, q;
+int a[N][N], b[N][N];
+
+void insert(int x1, int y1, int x2, int y2, int c)
+{
+    b[x1][y1] += c;
+    b[x2 + 1][y1] -= c;
+    b[x1][y2 + 1] -= c;
+    b[x2 + 1][y2 + 1] += c;
+}
+
+int main()
+{
+    scanf("%d%d%d", &n, &m, &q);
+
+    for (int i = 1; i <= n; i ++ )
+        for (int j = 1; j <= m; j ++ )
+            scanf("%d", &a[i][j]);
+
+    for (int i = 1; i <= n; i ++ )
+        for (int j = 1; j <= m; j ++ )
+            insert(i, j, i, j, a[i][j]);
+
+    while (q -- )
+    {
+        int x1, y1, x2, y2, c;
+        cin >> x1 >> y1 >> x2 >> y2 >> c;
+        insert(x1, y1, x2, y2, c);
+    }
+
+    for (int i = 1; i <= n; i ++ )
+        for (int j = 1; j <= m; j ++ )
+            b[i][j] += b[i - 1][j] + b[i][j - 1] - b[i - 1][j - 1];
+
+    for (int i = 1; i <= n; i ++ )
+    {
+        for (int j = 1; j <= m; j ++ ) printf("%d ", b[i][j]);
+        puts("");
+    }
+
+    return 0;
+}
+
+// 一维差分
+int n, m;
+int a[N], b[N];
+
+void insert(int l, int r, int c)
+{
+    b[l] += c;
+    b[r + 1] -= c;
+}
+
+int main()
+{
+    scanf("%d%d", &n, &m);
+    for (int i = 1; i <= n; i ++ ) scanf("%d", &a[i]);
+
+    for (int i = 1; i <= n; i ++ ) insert(i, i, a[i]);
+
+    while (m -- )
+    {
+        int l, r, c;
+        scanf("%d%d%d", &l, &r, &c);
+        insert(l, r, c);
+    }
+
+    for (int i = 1; i <= n; i ++ ) b[i] += b[i - 1];
+
+    for (int i = 1; i <= n; i ++ ) printf("%d ", b[i]);
+}
