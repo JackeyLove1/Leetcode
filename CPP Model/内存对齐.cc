98febaa9c39801b7490da4c20ce3e9027e6092a3
JackeyLove1 <1572727410@qq.com>
2022-05-27 14:48:59 +0800

    2022-05-27

--- CPP Model/内存对齐.cc
+++ CPP Model/内存对齐.cc
@@ -1,6 +1,8 @@
 #define ALIGNMENT 8 // must be a power of 2
 #define ALIGN(size) (((size) + (ALIGNMENT-1)) & ~(ALIGNMENT-1))
-
+static constexpr size_t ALIGN8(size_t size) {
+    return static_cast<size_t>(size - 1) / 8 * 8 + 8;
+}
 #include <iostream>
 using namespace std;
 
