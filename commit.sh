9ae47153cc60bdbcefbe246333a2c44292c0a83d
fanhuanjie <fanhuanjie@bytedance.com>
2022-09-20 00:43:10 +0800

    2022-09-20 00:43:10

--- commit.sh
+++ commit.sh
@@ -1,6 +1,6 @@
 current=`date "+%Y-%m-%d %H:%M:%S"`  
-timeStamp=`date -d "$current" +%s` 
-echo $current
+# timeStamp=`date -d "$current" +%s` 
+echo "commit time is $current"
 git add .
 git commit -m "$current"
 git push
