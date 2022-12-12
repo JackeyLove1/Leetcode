8410209d53b56822298085f73407c985565ce71f
fanhuanjie <fanhuanjie@bytedance.com>
2022-12-12 22:39:42 +0800

    2022-12-12 22:39:41

--- ACM/template/图论/最小瓶颈路/kruskal生成树.cc
+++ ACM/template/图论/最小瓶颈路/kruskal生成树.cc
@@ -108,3 +108,101 @@ int main() {
 
     return 0;
 }
+
+/*
+#include <bits/stdc++.h>
+#define MAXN 100005
+#define MOD 1000000007
+using namespace std;
+struct Edge {
+    int u, v, w, nxt;
+    bool operator<(const Edge t) const {
+        return w < t.w;
+    }
+} t[MAXN], e[MAXN << 2];
+int n, m, k, head[MAXN << 1], pos, u, v, idx, cnt, f[MAXN << 1], val[MAXN << 1];
+int A, B, C, P, ans, dep[MAXN << 2], seq[MAXN << 2], in[MAXN << 2], st[21][MAXN << 2];
+void addEdge(int u, int v, int w) {
+    e[++pos] = {u, v, w, head[u]};
+    head[u] = pos;
+}
+int find(int x) {
+    return f[x] == x ? x : f[x] = find(f[x]);
+}
+void build() {
+    sort(t + 1, t + m + 1);
+    idx = n;
+
+    for (int i = 1, cnt = 0; cnt < n - 1 && i <= m; i++)
+        if (find(t[i].u) != find(t[i].v)) {
+            int fa = find(t[i].u), fb = find(t[i].v);
+            f[fa] = f[fb] = ++idx;
+            cnt++;
+            addEdge(fa, idx, 0), addEdge(idx, fa, 0);
+            addEdge(fb, idx, 0), addEdge(idx, fb, 0);
+            val[idx] = t[i].w;
+        }
+}
+void dfs(int u, int f = 0, int d = 1) {
+    seq[++cnt] = u, dep[cnt] = d, in[u] = cnt;
+
+    for (int i = head[u]; i; i = e[i].nxt) {
+        if (e[i].v == f)
+            continue;
+
+        dfs(e[i].v, u, d + 1);
+        seq[++cnt] = u, dep[cnt] = d;
+    }
+}
+
+void init() {
+    for (int i = 1; i <= cnt; i++)
+        st[0][i] = i;
+
+    for (int i = 1; i <= __lg(cnt); i++)
+        for (int j = 1; j + (1 << i) - 1 <= cnt; j++) {
+            int l = st[i - 1][j], r = st[i - 1][j + (1 << (i - 1))];
+            st[i][j] = dep[l] < dep[r] ? l : r;
+        }
+}
+int LCA(int u, int v) {
+    int l = in[u], r = in[v];
+
+    if (l > r)
+        swap(l, r);
+
+    int s = __lg(r - l + 1);
+    int a = st[s][l], b = st[s][r - (1 << s) + 1];
+    return dep[a] < dep[b] ? seq[a] : seq[b];
+}
+
+inline int rnd() {
+    return A = (A * B + C) % P;
+}
+int main() {
+#ifndef ONLINE_JUDGE
+    freopen("data/data1.in", "r", stdin);
+    // freopen("data/data1.out", "w", stdout);
+#endif
+    scanf("%d%d", &n, &m);
+
+    for (int i = 1; i <= 2 * n; i++)
+        f[i] = i;
+
+    for (int i = 1; i <= m; i++)
+        scanf("%d%d%d", &t[i].u, &t[i].v, &t[i].w);
+
+    build();
+    dfs(idx);
+    init();
+    scanf("%d%d%d%d%d", &k, &A, &B, &C, &P);
+
+    while (k--) {
+        int u = rnd() % n + 1, v = rnd() % n + 1;
+        ans = (ans + val[LCA(u, v)]) % MOD;
+    }
+
+    printf("%d", ans);
+    return 0;
+}
+*/
--- ACM/template/未分类/std.cc
+++ ACM/template/未分类/std.cc
@@ -1,4 +1,88 @@
 // 求二进制中1的个数
 std::popcount(x) // C++20
 __builtin_popcount 
-// 
+
+// fast read/write
+namespace IO {
+template<typename T>inline bool read(T &x) {
+    x = 0;
+    char ch = getchar();
+    bool flag = 0, ret = 0;
+
+    while (ch < '0' || ch > '9')
+        flag = flag || (ch == '-'), ch = getchar();
+
+    while (ch >= '0' && ch <= '9')
+        x = x * 10 + ch - '0', ch = getchar(), ret = 1;
+
+    x = flag ? -x : x;
+    return ret;
+}
+template<typename T, typename ...Args>inline bool read(T &a, Args &...args) {
+    return read(a) && read(args...);
+}
+template<typename T>void prt(T x) {
+    if (x > 9)
+        prt(x / 10);
+
+    putchar(x % 10 + '0');
+}
+template<typename T>inline void put(T x) {
+    if (x < 0)
+        putchar('-'), x = -x;
+
+    prt(x);
+}
+template<typename T>inline void put(char ch, T x) {
+    if (x < 0)
+        putchar('-'), x = -x;
+
+    prt(x);
+    putchar(ch);
+}
+template<typename T, typename ...Args>inline void put(T a, Args ...args) {
+    put(a);
+    put(args...);
+}
+template<typename T, typename ...Args>inline void put(const char ch, T a, Args ...args) {
+    put(ch, a);
+    put(ch, args...);
+}
+inline void put(string s) {
+    for (int i = 0, sz = s.length(); i < sz; i++)
+        putchar(s[i]);
+}
+inline void put(const char *s) {
+    for (int i = 0, sz = strlen(s); i < sz; i++)
+        putchar(s[i]);
+}
+}
+using namespace IO;
+
+static constexpr int N = 1e6 + 100;
+using ll = long long;
+
+// 并查集
+class DSU {
+public:
+    DSU() {
+        for (int i = 1; i < N * 2; ++i)
+            fa[i] = i;
+    }
+    int find(int x) {
+        return fa[x] == x ? x : fa[x] = find(fa[x]);
+    }
+    inline void unity(int x, int y) {
+        fa[find(x)] = find(y);
+    }
+private:
+    int fa[N << 1];
+} dsu;
+
+// 边
+class Edge {
+public:
+    int u, v;
+    ll w;
+} e[N << 1];
+
