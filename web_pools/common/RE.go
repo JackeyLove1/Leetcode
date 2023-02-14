8a264667d403ac0af105350b151a47729cbf0ac7
JackeyLove1 <1572727410@qq.com>
2023-02-14 19:47:32 +0800

    2023-02-14 19:47:32

--- web_pools/common/RE.go
+++ web_pools/common/RE.go
@@ -11,4 +11,6 @@ str1 := "this is a [sample] [[string]] with [SOME] special words"
 		element = strings.Trim(element, "]")
 		fmt.Println(element)
 	}
-	
+// match phone number
+re := regexp.MustCompile(`^(?:(?:\(?(?:00|\+)([1-4]\d\d|[1-9]\d?)\)?)?[\-\.\ \\\/]?)?((?:\(?\d{1,}\)?[\-\.\ \\\/]?){0,})(?:[\-\.\ \\\/]?(?:#|ext\.?|extension|x)[\-\.\ \\\/]?(\d+))?$`)
+
