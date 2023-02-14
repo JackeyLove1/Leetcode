21a03a1496c25443c3e33e7c42a30414b2edfe2e
JackeyLove1 <1572727410@qq.com>
2023-02-14 19:39:19 +0800

    2023-02-14 19:39:18

--- web_pools/files/file.go
+++ web_pools/files/file.go
@@ -197,4 +197,47 @@ zipReader, _ := zip.OpenReader("test.zip")
 		}
 	}
 
+// write text file 
+sampledata := []string{"Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+		"Nunc a mi dapibus, faucibus mauris eu, fermentum ligula.",
+		"Donec in mauris ut justo eleifend dapibus.",
+		"Donec eu erat sit amet velit auctor tempus id eget mauris.",
+	}
+ 
+	file, err := os.OpenFile("test.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+ 
+	if err != nil {
+		log.Fatalf("failed creating file: %s", err)
+	}
+ 
+	datawriter := bufio.NewWriter(file)
+ 
+	for _, data := range sampledata {
+		_, _ = datawriter.WriteString(data + "\n")
+	}
+ 
+	datawriter.Flush()
+	file.Close()
+
+// read csv
+file, err := os.Open("test.txt")
+ 
+	if err != nil {
+		log.Fatalf("failed opening file: %s", err)
+	}
+ 
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanLines)
+	var txtlines []string
+ 
+	for scanner.Scan() {
+		txtlines = append(txtlines, scanner.Text())
+	}
+ 
+	file.Close()
+ 
+	for _, eachline := range txtlines {
+		fmt.Println(eachline)
+	}
+
 // 
