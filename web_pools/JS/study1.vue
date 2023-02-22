7e7940a0c59a31e2075de4ef19bc46a4131bd3f4
fanhuanjie <fanhuanjie@bytedance.com>
2023-02-22 23:59:34 +0800

    2023-02-22 23:59:33

---
+++ web_pools/JS/study1.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { reactive, ref } from 'vue'
+
+// 组件逻辑
+// 此处声明一些响应式状态
+const counter = reactive({couter:0})
+const message = ref("Hello, World!")
+const titleClass = ref('title')
+</script>
+
+<template>
+  <h1>{{message.split('').reverse().join('')}}</h1>
+  <p>Counter is:{{counter.couter}}</p>
+  <h1 :id="titleClass">Make Me Red</h1>
+</template>
+
+<style>
+#title{
+  color: red;
+}
+</style>
---
+++ web_pools/消息队列/nats/nats安装及简单使用.go
@@ -0,0 +1,2 @@
+// go get github.com/nats-io/nats.go/@latest
+// go get github.com/nats-io/nats-server/v2
